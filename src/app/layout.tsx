9b28fbe062cded32719a6f940a593eb566afea88
ViniciusMendesp <vmp_tech@outlook.com>
2024-02-18 13:03:27 -0300

    feat: :sparkles: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
+      <head>
+        <link rel="icon" href="/logo-jordan.svg" sizes="any" />
+      </head>
       <body className={archivo.className}>{children}</body>
     </html>
   );
